4a886217ccb4cfd22e9f97a71dc24221a3cf1f9c
Utkarsh Padariya <utkarsh@Utkarshs-MacBook-Pro.local>
2023-03-21 19:50:25 +0530

    updated build

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,59 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+import java.lang.Math;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Menu");
+            System.out.println("1. Square root function");
+            System.out.println("2. Factorial function");
+            System.out.println("3. Natural logarithm");
+            System.out.println("4. Power function");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a number: ");
+                    double sqrtNum = sc.nextDouble();
+                    System.out.println("The square root of " + sqrtNum + " is " + Math.sqrt(sqrtNum));
+                    break;
+                case 2:
+                    System.out.print("Enter a number: ");
+                    int factNum = sc.nextInt();
+                    int factorial = 1;
+                    for (int i = 1; i <= factNum; i++) {
+                        factorial *= i;
+                    }
+                    System.out.println(factNum + "! = " + factorial);
+                    break;
+                case 3:
+                    System.out.print("Enter a number: ");
+                    double lnNum = sc.nextDouble();
+                    System.out.println("The natural logarithm of " + lnNum + " is " + Math.log(lnNum));
+                    break;
+                case 4:
+                    System.out.print("Enter a number: ");
+                    double base = sc.nextDouble();
+                    System.out.print("Enter the exponent: ");
+                    double exponent = sc.nextDouble();
+                    System.out.println(base + "^" + exponent + " = " + Math.pow(base, exponent));
+                    break;
+                case 5:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid choice.");
+                    break;
+            }
+        } while (choice != 5);
+        sc.close();
     }
 }
+
+
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,20 +1,45 @@
 package com.example;
 
-import static org.junit.Assert.assertTrue;
+
 
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import static org.junit.Assert.*;
+
+public class AppTest {
+    
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testSquareRootFunction() {
+        double expectedResult = 4.0;
+        double actualResult = Math.sqrt(16.0);
+        assertEquals(expectedResult, actualResult, 0.001);
+    }
+    
+    @Test
+    public void testFactorialFunction() {
+        int expectedResult = 120;
+        int actualResult = 1;
+        for (int i = 1; i <= 5; i++) {
+            actualResult *= i;
+        }
+        assertEquals(expectedResult, actualResult);
+    }
+    
+    @Test
+    public void testNaturalLogarithmFunction() {
+        double expectedResult = 1.791759469228055;
+        double actualResult = Math.log(6.0);
+        assertEquals(expectedResult, actualResult, 0.001);
+    }
+    
+    @Test
+    public void testPowerFunction() {
+        double expectedResult = 16.0;
+        double actualResult = Math.pow(4.0, 2.0);
+        assertEquals(expectedResult, actualResult, 0.001);
     }
 }
+
